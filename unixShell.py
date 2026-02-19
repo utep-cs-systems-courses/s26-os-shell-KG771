d90eb645116b6428292ded20df02f62ca4cc46d4
Karen <kgarcia771@outlook.com>
2026-02-19 16:13:36 -0700

    updated

--- unixShell.py
+++ unixShell.py
@@ -7,19 +7,35 @@ import re
 
 command = ''
 
-def checkValidCommand(command):
-    if command != '':
-        return command
-    else:
-        print(command + ": command not found")
-
-        
- 
-childPID = os.fork()
-PS1 = os.environ.get("PS1")
+def verifyCommand(command):
+    return command
 
 while True:
-    if PS1:
-        print("PS1 " + command)
+    commandPrompt = os.environ.get("PS1", "$")
+    try:
+        userInput = input(commandPrompt).strip()
+    except EOFError:
+        sys.exit(0)
+
+    #handles case where user enters without input
+    if userInput == '':
+        continue
+
+    if userInput == 'exit':
+        sys.exit(0)
+    elif userInput.startswith('cd'):
+        os.chdir()
+    elif '>' in userInput:
+        #redirection
+        PID = os.fork()
+    elif '<' in userInput:
+        #redirection
+        PID = os.fork()
+    elif '|' in userInput:
+        #pipe
+        PID = os.fork()
     else:
-        print("$ " + command)
+        #handle simple command
+        pass
+        
+        
