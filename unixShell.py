b188f7db710d5c7673f4b69456d0f81c1503773e
Karen <kgarcia771@outlook.com>
2026-02-20 20:59:55 -0700

    updated

--- unixShell.py
+++ unixShell.py
@@ -36,8 +36,31 @@ while True:
                 print("cd: no such file or directory: " + path[1])
     
     elif '>' in userInput:
-        #redirection
-        PID = os.fork()
+        # split on '>' to get command and filename
+        parts = userInput.split('>')
+        args = parts[0].strip().split()
+        filename = parts[1].strip()
+        
+        path = findPath(args[0])
+        if path is None:
+            print(args[0] + ": command not found")
+        else:
+            PID = os.fork()
+            if PID == 0:
+                # child process
+                fd = os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o644)
+                # 2. replace stdout (fd 1) with the file
+                os.dup2(fd, 1)
+                # 3. close the original file descriptor
+                os.close(fd)
+                # 4. exec the command
+                os.execve(path, args, os.environ)
+            else:
+                # parent - same as simple command
+                _, status = os.wait()
+                exitCode = os.WEXITSTATUS(status)
+                if exitCode != 0:
+                    print("Program terminated with exit code " + str(exitCode))
     
     elif '<' in userInput:
         #redirection
@@ -48,6 +71,21 @@ while True:
         PID = os.fork()
     else:
         #handle simple command
-        pass
+        args = userInput.split()
+        path = findPath(args[0])
+        if path is None:
+            print(args[0] + ": command not found")
+        else:
+            PID = os.fork()
+            if PID == 0:
+                # child process - execute the command
+                os.execve(path, args, os.environ)
+            else:
+                # parent process - wait for child
+                _, status = os.wait()
+                # if exit code non-zero, print the error message
+                exitCode = os.WEXITSTATUS(status)
+                if exitCode != 0:
+                    print("Program terminated with exit code " + str(exitCode))
         
         
